692dfd5976cb381a50debe5e20b2c963a9305870
Machuka Joy <machukajoy@gmail.com>
2022-04-27 22:34:51 +0300

    ft(configurations): make our application use the configurations

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,10 @@
 from flask import Flask
+from .config import Devconfig
 
 #initialize application
 app = Flask(__name__)
+
+# setting up configuration
+app.config.from_object(Devconfig)
 
 from app import views
