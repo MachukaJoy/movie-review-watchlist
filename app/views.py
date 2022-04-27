2f0180cc35d2e34ff191c1b1c0872f931562cae6
Machuka Joy <machukajoy@gmail.com>
2022-04-27 20:19:42 +0300

    ft(display variable): add a variable in the views file for display

--- app/views.py
+++ app/views.py
@@ -1,3 +1,4 @@
+from email import message
 from flask import render_template
 from app import app
 
@@ -8,4 +9,5 @@ def index():
   '''
   View root page function that returns the index page and its data
   '''
-  return render_template('index.html')
+  message = "Hello Joy, keep pushing"
+  return render_template('index.html', message=message)
